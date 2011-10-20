1756e08e61f36f118e000a4844a1ac3f92440c9a
Maxime Dénès <Maxime.Denes@inria.fr>
2011-10-20 14:28:38 +0200

    Made infra.v compatible with Coq 8.3. Commented scripts for Coq >= 8.4.

--- infra.v
+++ infra.v
@@ -23,6 +23,8 @@ Open Local Scope ring_scope.
 
 (* Structures on positive *)
 
+(* For Coq >= 8.4 *)
+(*
 Definition eqp (p q : positive) : bool :=
   match (p ?= q)%positive with Eq => true | _ => false end.
 
@@ -30,6 +32,16 @@ Lemma eqpP : Equality.axiom eqp.
 Proof.
   move=> p q; apply: (iffP  idP)=>[|<-]; last by rewrite /eqp Pos.compare_refl.
   rewrite /eqp; case e: (p ?= q)%positive=> // _; exact: Pcompare_Eq_eq.
+Qed.
+*)
+
+Definition eqp (p q : positive) : bool :=
+ match ((p ?= q) Eq)%positive with Eq => true | _ => false end.
+
+Lemma eqpP : Equality.axiom eqp.
+Proof.
+  move=> p q; apply: (iffP  idP)=>[|<-]; last by rewrite /eqp Pcompare_refl.
+  rewrite /eqp; case e: ((p ?= q) Eq)%positive=> // _; exact: Pcompare_Eq_eq.
 Qed.
 
 Canonical Structure eqp_Mixin := EqMixin eqpP.
