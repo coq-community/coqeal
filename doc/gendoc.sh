ccb01ec318175900000b66e766df79e0ec7bda2e
Maxime Dénès <Maxime.Denes@inria.fr>
2013-02-15 19:18:22 +0100

    Adapting gendoc script (local use only)

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -4,7 +4,10 @@ for i in `ls ../*.v`
 do
   ~/formathdocs/software/coq2html/coq2html -o %.html $globs $i
 done
-coqdep -noglob -I . ../*.v > depend
+cd ..
+coqdep -noglob -I . *.v > depend
+sed s/[[:space:]][[:alnum:]]*.v.beautified//g depend > doc/depend
+cd doc
 ~/coqfinitgroup/trunk/doc/makeDot/makedot depend
 dot -Tpng -o depend.png -Tcmapx -o depend.map depend.dot
 dot -Tsvg -o depend.svg depend.dot
