88253aafe55af41d00df35e82150551e6617af20
Anders <mortberg@chalmers.se>
2013-02-15 18:05:06 +0100

    Clean up of karatsuba

--- v0.1/karatsuba.v
+++ v0.1/karatsuba.v
@@ -2,7 +2,7 @@
 (c) Copyright INRIA and University of Gothenburg. *)
 Require Import ssreflect ssrfun ssrbool eqtype ssrnat div seq.
 Require Import path choice fintype tuple finset ssralg poly.
-Require Import bigop cseqpoly cssralg.
+Require Import bigop cssralg cseqpoly.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -19,62 +19,31 @@ Variable CR : cringType R.
 Implicit Types p q : {poly R}.
 
 (* Splitting polynomials *)
-
 Definition splitp n p := (Poly (drop n p), Poly (take n p)).
 
-Lemma coef_oversize : forall p i, size p <= i -> p`_i = 0.
+Lemma splitP n p : p = (splitp n p).1 * 'X^n + (splitp n p).2.
 Proof.
-elim/poly_ind => [i | p c ih i]; first by rewrite coef0.
-case i0: (i == 0%N).
-  move/eqP: i0 => ->.
-  rewrite leqn0 size_poly_eq0 => /eqP ->.
-  by rewrite coef0.
-rewrite -cons_poly_def size_cons_poly coef_cons=> h.
-rewrite i0 ih //.
-move: h.
-case: ifP => [|_ spi].
-  by case/andP; rewrite nil_poly=> /eqP ->; rewrite size_poly0 !leq0n.
-by rewrite -ltnS (prednK (neq0_lt0n i0)).
+apply/polyP=> i; rewrite coefD coefMXn !coef_Poly nth_drop addnC.
+have [i_lt_n|n_leq_i] := ltnP; first by rewrite add0r nth_take.
+rewrite subnK ?[(take _ _)`_i]nth_default ?addr0 // size_take.
+by have [//|sp_leq_n] := ltnP; apply/(leq_trans sp_leq_n).
 Qed.
-
-Lemma splitP : forall n p, p = (splitp n p).1 * 'X^n + (splitp n p).2.
-Proof.
-move=> n p /=.
-apply/polyP=> i.
-rewrite coefD coefMXn.
-case: ifP => hin.
-  by rewrite coef_Poly add0r nth_take.
-rewrite {1}coef_Poly nth_drop addnC subnK; last first.
-  by rewrite leqNgt hin.
-move/negP: hin => /negP.
-rewrite -leqNgt=> h.
-case: (ltnP n (size p)) => sh.
-  have size_take: (size (Poly (take n p)) <= i).
-    move: (size_Poly (take n p)).
-    move: (size_take n p).
-    rewrite sh => -> h1.
-    by rewrite (leq_trans h1).
-  by rewrite (@coef_oversize (Poly (take n p))) // addr0.
-by rewrite coef_Poly take_oversize // coef_oversize ?addr0 // (leq_trans sh).
-Qed.
-
-(* Karatsuba multiplication *)
-
-
 
 (* Karatsuba multiplication *)
 Fixpoint karatsuba_rec (n : nat) p q := match n with
   | 0%N   => p * q
-  | n'.+1 => if (size p <= 2) || (size q <= 2) then p * q else
-      let m       := minn (size p)./2 (size q)./2 in
-      let (p1,p2) := splitp m p in
-      let (q1,q2) := splitp m q in
-      let p1q1    := karatsuba_rec n' p1 q1 in
-      let p2q2    := karatsuba_rec n' p2 q2 in
-      let p12     := p1 + p2 in
-      let q12     := q1 + q2 in
-      let p12q12  := karatsuba_rec n' p12 q12 in
-      p1q1 * 'X^(2 * m) + (p12q12 - p1q1 - p2q2) * 'X^m + p2q2
+  | n'.+1 => 
+      let sp := size p in let sq := size q in 
+      if (sp <= 2) || (sq <= 2) then p * q else
+        let m       := minn sp./2 sq./2 in
+        let (p1,p2) := splitp m p in
+        let (q1,q2) := splitp m q in
+        let p1q1    := karatsuba_rec n' p1 q1 in
+        let p2q2    := karatsuba_rec n' p2 q2 in
+        let p12     := p1 + p2 in
+        let q12     := q1 + q2 in
+        let p12q12  := karatsuba_rec n' p12 q12 in
+        p1q1 * 'X^(2 * m) + (p12q12 - p1q1 - p2q2) * 'X^m + p2q2
   end.
 
 Definition karatsuba p q :=
@@ -84,41 +53,24 @@ Definition karatsuba p q :=
 Lemma karatsuba_recP : forall n p q, karatsuba_rec n p q = p * q.
 Proof.
 elim=> // n ih p q /=.
-case: ifP=> // sp.
-apply/eqP.
+case: ifP=> // _.
 set m := minn (size p)./2 (size q)./2.
-rewrite eq_sym {1}(splitP m p) {1}(splitP m q) /=.
-rewrite !ih; do ?(by rewrite ?hm ?doubleK).
-set p1 := (Poly (drop m p)).
-set p2 := (Poly (take m p)).
-set q1 := (Poly (drop m q)).
-set q2 := (Poly (take m q)).
-rewrite !mulrDl !mulrDr -!mulrA ['X^m * (_ * _)]mulrCA -exprD addnn
-        -mul2n.
-apply/eqP.
-rewrite -!addrA.
-congr (_ + _).
-rewrite mulrDl -!mulrA [p1 * _+ p1 * _]addrC ['X^m * q2]mulrC -!addrA.
-congr (_ + _).
-rewrite mulrDl -!addrA addrCA -!mulrA.
-congr (_ + _).
-rewrite !addrA -{1}[p2*q2]add0r.
-congr (_ + _).
-apply/eqP.
-rewrite eq_sym addr_eq0 -addrA addrCA addrC -addrA eq_sym addrC -subr_eq.
-by rewrite -!mulrN -!mulrA mulrNN mulrN mulNr addrC.
+rewrite {7}(splitP m p) {7}(splitP m q) /= !ih.
+set p1 := (Poly (drop m p)); set p2 := (Poly (take m p)).
+set q1 := (Poly (drop m q)); set q2 := (Poly (take m q)).
+rewrite [(p1 + p2) * _]mulrDl [p1 * (_ + _)]mulrDr [p2 * (_ + _)]mulrDr.
+rewrite -!addrA [_ - p2 * q2]addrC addNKr [p1 * _ + _]addrCA [_ - p1 * q1]addrC.
+rewrite addNKr mulrDr !mulrDl -!mulrA ['X^m * (q1 * _)]mulrCA -exprD addnn.
+by rewrite -mul2n ['X^m * q2]mulrC -!addrA [_ + (_ + p2 * _)]addrCA.
 Qed.
 
-
-Lemma karatsubaP : forall p q, karatsuba p q = p * q.
+Lemma karatsubaP p q : karatsuba p q = p * q.
 Proof.
-move=> p q.
-rewrite /karatsuba.
-wlog sqp : p q / size q <= size p=> [H|].
-  case: ltnP=> spq.
-    by move/H: (ltnW spq); rewrite ltnNge (ltnW spq) mulrC.
-  by move/H: (spq); rewrite ltnNge spq.
-by rewrite ltnNge sqp /= karatsuba_recP.
+rewrite /karatsuba ltnNge.
+wlog -> : p q / size q <= size p=> [H|].
+  have [spq|spq] := ltnP; first by move/H: (ltnW spq); rewrite (ltnW spq) mulrC.
+  by move/H: (spq); rewrite spq.
+by rewrite /= karatsuba_recP.
 Qed.
 
 
@@ -130,7 +82,6 @@ Fixpoint take' n (s : seq CR) :=  match n, s with
                       in if size r != 0%N then x :: r else nseq (x != zero CR) x
   | _, _ => [::]
   end.
-
 
 Lemma last_drop : forall (p : seq R) n, last 1 p != 0 -> last 1 (drop n p) != 0.
 Proof.
@@ -146,8 +97,7 @@ Lemma splitp_seqE p n :
   (trans (splitp n p).1,trans (splitp n p).2) = splitp_seq n (trans p).
 Proof.
 rewrite /splitp_seq /=.
-apply/pair_eqP => /=.
-apply/andP.
+apply/pair_eqP/andP => /=.
 case: p => p /= h.
 split; first by rewrite trans_poly_def (@PolyK _ 1) ?map_drop // last_drop.
 rewrite [trans (Polynomial h)]trans_poly_def /=.
@@ -161,17 +111,19 @@ Qed.
 
 Fixpoint karatsuba_rec_seq (n : nat) (p q : seq CR) := match n with
   | 0%N   => mul_seq p q
-  | n'.+1 => if (size p <= 2) || (size q <= 2) then mul_seq p q else
-      let m       := minn (size p)./2 (size q)./2 in
-      let (p1,p2) := splitp_seq m p in
-      let (q1,q2) := splitp_seq m q in
-      let p1q1    := karatsuba_rec_seq n' p1 q1 in
-      let p2q2    := karatsuba_rec_seq n' p2 q2 in
-      let p12     := add_seq p1 p2 in
-      let q12     := add_seq q1 q2 in
-      let p12q12  := karatsuba_rec_seq n' p12 q12 in
-      add_seq (add_seq (shift (2 * m) p1q1)
-              (shift m (sub_seq (sub_seq p12q12 p1q1) p2q2))) p2q2
+  | n'.+1 => 
+      let sp := size p in let sq := size q in 
+      if (sp <= 2) || (sq <= 2) then mul_seq p q else
+        let m       := minn (size p)./2 (size q)./2 in
+        let (p1,p2) := splitp_seq m p in
+        let (q1,q2) := splitp_seq m q in
+        let p1q1    := karatsuba_rec_seq n' p1 q1 in
+        let p2q2    := karatsuba_rec_seq n' p2 q2 in
+        let p12     := add_seq p1 p2 in
+        let q12     := add_seq q1 q2 in
+        let p12q12  := karatsuba_rec_seq n' p12 q12 in
+        add_seq (add_seq (shift (2 * m) p1q1)
+                 (shift m (sub_seq (sub_seq p12q12 p1q1) p2q2))) p2q2
   end.
 
 Definition karatsuba_seq (p q : seq CR) :=
