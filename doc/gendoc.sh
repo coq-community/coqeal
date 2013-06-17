8c18bb65519f1bb3f43811ebee3fc493b8ce891e
Maxime Dénès <Maxime.Denes@inria.fr>
2013-06-17 09:07:55 +0200

    Avoid relative paths in gendoc.sh.

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+COQ2HTML=$COQEAL/../coq2html
+make -C makeDot
+make -C $COQ2HTML
+cd $COQEAL/doc
 globs=`ls ../*.glob`
 for i in `ls ../*.v`
 do
-  ~/formathdocs/software/coq2html/coq2html -o %.html $globs $i
+  $COQ2HTML/coq2html -o %.html $globs $i
 done
-cd ..
-coqdep -noglob -I . *.v > depend
-sed s/[[:space:]][[:alnum:]]*.v.beautified//g depend > doc/depend
-cd doc
-~/coqfinitgroup/trunk/doc/makeDot/makedot depend
+cd $COQEAL
+$COQBIN/coqdep -noglob -I . -R $SSRLIB Ssreflect *.v > depend
+#sed s/[[:space:]][[:alnum:]]*.v.beautified//g depend > doc/depend
+cd $COQEAL/doc
+$WORKSPACE/makeDot/makedot depend
 dot -Tpng -o depend.png -Tcmapx -o depend.map depend.dot
 dot -Tsvg -o depend.svg depend.dot
 cat toc.html.head depend.map toc.html.tail > toc.html
