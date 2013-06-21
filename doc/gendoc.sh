9afa77e00f01de6e6fa8441b9ccdde22ece8894b
Maxime Dénès <Maxime.Denes@inria.fr>
2013-06-21 13:02:28 +0200

    Fixed links in CoqEAL doc.

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -9,7 +9,7 @@ globs=`ls ../*.glob`
 VFILES=`grep -e "^[^ ]*\.v" ../Make`
 for i in $VFILES
 do
-  $COQ2HTML/coq2html -o %.html $globs ../$i
+  $COQ2HTML/coq2html -R CoqEAL -o %.html $globs ../$i
 done
 cd $COQEAL
 echo $VFILES | xargs $COQBIN/coqdep -noglob -I . -R $SSRLIB Ssreflect > depend
@@ -19,4 +19,3 @@ $WORKSPACE/makeDot/makedot depend
 dot -Tpng -o depend.png -Tcmapx -o depend.map depend.dot
 dot -Tsvg -o depend.svg depend.dot
 cat toc.html.head depend.map toc.html.tail > toc.html
-#cp *.html depend.* /net/servers/www-sop/members/Maxime.Denes/coqeal
