7c3067494d2c15008b77b14fe5c47997efbadf95
Maxime Dénès <mail@maximedenes.fr>
2014-04-17 16:47:56 -0400

    Trying to understand example on seqmatrix.

--- refinements/examples/seqmatrix.v
+++ refinements/examples/seqmatrix.v
@@ -4,13 +4,28 @@ Require Import ssreflect ssrfun ssrbool eqtype ssrnat div seq zmodp.
 Require Import path choice fintype tuple finset fingroup perm ssralg bigop matrix mxalgebra.
 Require Import refinements.
 
-Require Import ZArith ssrint binint seqmatrix.
+Require Import ZArith ssrint hrel binint seqmatrix.
+
+Require binnat.
 
 Open Scope ring_scope.
 
-Definition M := \matrix_(i,j < 2) 1%:Z.
-Definition N := \matrix_(i,j < 2) 2%:Z.
-Definition P := \matrix_(i,j < 2) 14%:Z.
+Definition M := \matrix_(i,j < 2) 1%num%:Z.
+Definition N := \matrix_(i,j < 2) 2%num%:Z.
+Definition P := \matrix_(i,j < 2) 14%num%:Z.
+
+Set Typeclasses Debug.
+
+Goal - P = - P.
+Proof.
+apply/eqP.
+erewrite param_eq; last first.
+eapply param_apply.
+eapply param_apply.
+
+(* We does the following pick the instance binnat.eq_N ? *)
+eapply @RseqmxA_eqseqmx.
+Abort.
 
 Goal (M + N + M + N + M + N + N + M + N) *m
    (M + N + M + N + M + N + N + M + N) = 
