1cdbbef471e32806280d4d8459959cc748b7f90b
mlasson <marc.lasson@gmail.com>
2014-11-21 12:51:53 +0100

    bug.v

---
+++ test-suite/Parametricity.v
@@ -0,0 +1,4 @@
+Declare ML Module "relations".
+Declare ML Module "parametricity".
+Declare ML Module "declare_translation".
+Declare ML Module "abstraction". 
---
+++ test-suite/bug.v
@@ -0,0 +1,18 @@
+Require Import Parametricity.
+
+Set Printing Implicit.
+
+Translate Inductive eq.
+
+Translate Inductive eq_R.
+
+Translate Inductive nat. 
+Translate Inductive nat_R. 
+Translate Inductive nat_R_R. 
+
+Translate Nat.add.
+
+ 
+Translate add_R.
+Translate add_R_R. 
+
