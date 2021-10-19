c83b3f9c96e30cdb7a1cc9330eb23a505287e178
Karl Palmskog <palmskog@gmail.com>
2021-10-19 22:42:37 +0200

    add all Coq files except similar.v from https://github.com/coq-community/matrix_canonical_forms/tree/86cc2a060630faddb91a8360783667fbd419756f

---
+++ theory/companion.v
@@ -0,0 +1,147 @@
+From mathcomp Require Import all_ssreflect.
+From mathcomp Require Import all_algebra.
+From CoqEAL Require Import ssrcomplements mxstructure.
+
+(**  This file defines companion matrices for any non-constant polynomial and
+  prooves the properties of their characteristic and minimal polynomials
+
+     companion_mx p == The companion matrix of the polynomial p.
+
+                                                                              *)
+
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section Companion.
+
+Local Open Scope ring_scope.
+Import GRing.Theory.
+
+Variable R : comRingType.
+
+Definition companion_mxn n (p : {poly R}) :=
+  \matrix_(i, j < n ) ((i == j.+1 :> nat)%:R
+    - p`_i *+ ((size p).-2 == j)).
+
+Definition companion_mx (p : {poly R}) := companion_mxn (size p).-2.+1 p.
+
+Lemma comp_char_polyK : forall (p : {poly R}), p \is monic -> 
+  (1 < size p)%N -> char_poly (companion_mx p) = p.
+Proof.
+apply: poly_ind=> [|p c IHp]; first by move/monic_neq0/eqP.
+case: (p == 0) /eqP => [-> H |/eqP H Hm Hs].
+  by rewrite mul0r add0r {1}size_polyC; case: (c != 0).
+have Hcst1 : (size (p * 'X + c%:P)).-1 = (size p).-1.+1.
+  by rewrite size_MXaddC (negbTE H) -polySpred.
+have Hmp : p \is monic.
+  rewrite monicE -lead_coefMX -(@lead_coefDl _ _ (c%:P)) -?monicE //. 
+  by rewrite size_polyC size_mulX // polySpred //; case:(c != 0).
+case: (ltnP 1 (size p))=> Hpt; last first.
+  have Hp1: p = 1%:P by rewrite -(monicP Hmp) [p]size1_polyC // lead_coefC.
+  rewrite /companion_mx !Hcst1 Hp1 mul1r /char_poly size_polyC oner_eq0. 
+  set M := char_poly_mx _.  
+  rewrite [M]mx11_scalar det_scalar1 !mxE coefD coefC coefX.
+  by rewrite !add0r polyCN opprK size_XaddC.  
+rewrite /char_poly /companion_mx Hcst1.
+rewrite (expand_det_row _ ord0) big_ord_recl !mxE.
+rewrite mulr1n !mulr0n add0r /cofactor !addn0 expr0 mul1r.
+set d1 := \det _.
+case Hnp: (size p) (Hpt)=> [|n] //; case: n Hnp=> // n Hnp _. 
+rewrite big_ord_recr big1; last first.
+   move=> i _; rewrite !mxE !sub0r size_MXaddC (negbTE H) andFb. 
+   have:= (neq_ltn n (widen_ord (leqnSn n) i)).
+   rewrite Hnp (ltn_ord i) orbT lift0 eqSS.
+   by move/negbTE ->; rewrite polyCN opprK mul0r.
+rewrite /= add0r; set M := row' _ _.
+have HM: upper_triangular_mx M.
+  apply/upper_triangular_mxP=> i j Hij.
+  rewrite !mxE -(inj_eq (@ord_inj _)) /= /bump !leq0n leqNgt (ltn_ord j).
+  rewrite add1n eqn_leq leqNgt ltnS ltnW // sub0r eqSS eqn_leq leqNgt Hij.
+  rewrite sub0r eqn_leq size_MXaddC (negbTE H) andFb Hnp.
+  by rewrite  (leqNgt n.+1) (ltn_ord j) polyCN opprK.
+have->: \det M = (-1)^+n.+1.
+  rewrite (det_triangular_mx HM) -{7}[n.+1]card_ord -prodr_const.
+  apply: eq_bigr=> i _; rewrite !mxE -(inj_eq (@ord_inj _)) !lift0 !lift_max.
+  rewrite eqxx !eqn_leq ltnn size_MXaddC (negbTE H) andFb Hnp. 
+  by rewrite (leqNgt _ i) (ltn_ord i) sub0r subr0.
+rewrite !mxE -exprD -signr_odd addnn odd_double mulr1 polyCN opprK.
+rewrite size_MXaddC (negbTE H) andFb Hnp addr0 !sub0r.
+rewrite -{1}cons_poly_def coef_cons polyCN opprK !eqxx -(IHp Hmp Hpt) mulrC.
+suff ->: d1 = char_poly (companion_mx p)=> //.
+rewrite /companion_mx.
+have ->: (size p).-2.+1 = (size p).-1.+1.-1.+1.-1 by rewrite Hnp.
+congr (\det _); rewrite row'_col'_char_poly_mx; congr char_poly_mx.
+apply/matrixP=> i j; rewrite !mxE !eqSS -cons_poly_def coef_cons size_cons_poly.
+rewrite nil_poly (negbTE H).
+by rewrite !lift0 /= {4 9}Hnp.
+Qed.
+
+End Companion.
+
+Section CompanionMin.
+
+Variable F : fieldType.
+Local Open Scope ring_scope.
+Import GRing.Theory.
+
+Lemma comp_mxminpolyK :  forall (p : {poly F}), p \is monic ->
+  (1 < size p)%N -> mxminpoly (companion_mx p) = p.
+Proof.
+move=> p Hp Hs.
+set A := companion_mx p.
+suff Hn: forall q, horner_mx A q = 0 ->
+    (q == 0) || ((size p).-2 < (size q).-1)%N.
+  have Hm0: (mxminpoly A == 0) = false.
+    by apply: negbTE; rewrite monic_neq0 // mxminpoly_monic.
+  have:= (Hn (mxminpoly A) (mx_root_minpoly A)); rewrite Hm0 /= => Hmn.
+  have Hsm : size (mxminpoly A) == size (char_poly A).
+   rewrite eqn_leq dvdp_leq ?mxminpoly_dvd_char ?monic_neq0 ?char_poly_monic //.
+   by rewrite size_char_poly -(addn1 _.-2) addnC -ltn_subRL subn1. 
+  apply/eqP; rewrite -eqp_monic // ?mxminpoly_monic //.
+  by rewrite -{2}(comp_char_polyK Hp) // -dvdp_size_eqp // mxminpoly_dvd_char.
+move=> q; case: (ltnP (size p).-2 (size q).-1); first by rewrite orbT.
+have H (i : 'I_(size p).-2): 
+  A *m col (widen_ord (leqnSn (size p).-2) i) 1%:M = col (lift ord0 i) 1%:M.  
+  rewrite col_id_mulmx; apply/matrixP=> j k; rewrite !mxE.
+  rewrite -(inj_eq (@ord_inj _)) lift0.
+  by rewrite (eqn_leq _ i) (leqNgt _ i) (ltn_ord i) subr0.
+have H2: forall i : 'I_(size p).-2.+1, (A ^+ i) *m col ord0 1%:M = col i 1%:M.
+  case; elim=> [Hi|i IH Hi] /=. 
+    by rewrite expr0 mul1mx; congr col; apply: ord_inj. 
+  rewrite exprS -mulmxA (IH (ltnW Hi)).
+  have Ho: (i <  (size p).-2)%N by rewrite -ltnS.
+  have ->: (Ordinal (ltnW Hi)) = (widen_ord (leqnSn (size p).-2) (Ordinal Ho)).
+    by apply: ord_inj.
+  by rewrite H; congr col; apply: ord_inj; rewrite lift0.
+case Hq: (q == 0)=> //.
+have Hsq: (0 < size q)%N by rewrite size_poly_gt0 Hq.
+rewrite /horner_mx /horner_morph horner_coef. 
+rewrite size_map_poly_id0 ?fmorph_eq0 ?lead_coef_eq0 ?Hq // => H1 Hb.
+have Hw: (size q <= (size p).-2.+1)%N by rewrite -(prednK Hsq).
+suff : q == 0 by rewrite Hq.
+have: \sum_(i < size q) q`_i *: (A ^+ i *m col ord0 1%:M) = 0.
+  rewrite (eq_bigr (fun i : 'I_(size q) => 
+                    ((map_poly scalar_mx q)`_i * A ^+ i) *m col ord0 1%:M)).
+    by rewrite -mulmx_suml ?Hb ?mul0mx //.
+  by move=> i _; rewrite coef_map scalemxAl -mul_scalar_mx.
+set b := \sum_(_ < _) _.
+have <-: \col_(i < (size p).-2.+1) q`_i = b.
+  apply/matrixP=> i j; rewrite mxE summxE.
+  case: (ltnP i (size q))=> Hi.
+    rewrite (bigD1 (Ordinal Hi)) //= H2 !mxE eqxx mulr1 big1 ?addr0 //.
+    move=> k Hk; rewrite (H2 (widen_ord Hw k)) !mxE.
+    move/negbTE: Hk; rewrite -!(inj_eq (@ord_inj _)) /= eq_sym=> ->.
+    by rewrite mulr0.      
+  rewrite nth_default // big1 // => k _.    
+  rewrite (H2 (widen_ord Hw k)) !mxE -(inj_eq (@ord_inj _)) /= eqn_leq.
+  by rewrite leqNgt (leq_trans (ltn_ord k) Hi) andFb mulr0.  
+move/matrixP=> Hc.
+apply/eqP/size_poly_leq0P/leq_sizeP=> j _. 
+case: (ltnP j (size p).-2.+1)=> Hj.
+  by move: (Hc (Ordinal Hj) ord0); rewrite !mxE.
+by rewrite nth_default //; apply: leq_trans Hj.
+Qed.
+
+End CompanionMin.
---
+++ theory/perm_eq_image.v
@@ -0,0 +1,234 @@
+From mathcomp Require Import all_ssreflect all_algebra.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section seq_eqType.
+
+Variable T1 : eqType.
+
+Lemma sorted_trans (leT1 leT2 : rel T1) s :
+  {in s &, (forall x y, leT1 x y -> leT2 x y)} ->
+  sorted leT1 s -> sorted leT2 s.
+Proof.
+elim: s=> // a [] //= b l IHl leT12 /andP [leT1ab pleT1].
+rewrite leT12 ?inE ?eqxx ?orbT // IHl // => x y xbcl ybcl leT1xy.
+  by rewrite leT12 // mem_behead.
+Qed.
+
+
+End seq_eqType.
+
+Section FinType.
+
+Lemma enum_ord_enum n : enum 'I_n = ord_enum n.
+Proof. by rewrite enumT unlock. Qed.
+
+End FinType.
+
+
+Section Finfun.
+
+Variables (aT : finType) (rT : eqType).
+Variables (f g : aT -> rT).
+Variable (P : pred aT).
+Hypothesis (Hf : injective f) (Hg : injective g).
+
+Lemma uniq_image (h : aT -> rT):
+  injective h -> uniq (image h P).
+Proof. by move/map_inj_uniq=> ->; rewrite enum_uniq. Qed.
+
+Lemma perm_eq_image :  {subset (image f P) <= (image g P)} ->
+  perm_eq (image f P) (image g P).
+Proof.
+move=> imfsubimg.
+rewrite uniq_perm // ?uniq_image //.
+have []:= (uniq_min_size (uniq_image Hf) imfsubimg)=> //.
+by rewrite !size_map.
+Qed.
+
+End Finfun.
+
+Section BigOp.
+
+Variables (T : Type) (idx : T) (op : Monoid.com_law idx).
+
+Lemma sumn_big s : sumn s = (\sum_(i <- s) i)%N.
+Proof.
+elim: s=> /= [|a l ->]; first by rewrite big_nil.
+by rewrite big_cons.
+Qed.
+(***Not in bigop.v and I not found a short way to prove this. ****)
+Lemma big_lift_ord n F j :
+  \big[op/idx]_( i < n.+1 | j != i ) F i = \big[op/idx]_i F (lift j i).
+Proof.
+case: (pickP 'I_n) => [k0 _ | n0]; last first.
+  by rewrite !big1 // => [k /unlift_some[i] | i _]; have:= n0 i.
+rewrite (reindex (lift j)).
+  by apply: eq_bigl=> k; rewrite neq_lift.
+exists (fun k => odflt k0 (unlift j k)) => k; first by rewrite liftK.
+by case/unlift_some=> k' -> ->.
+Qed.
+
+Variable R : idomainType.
+Open Scope ring_scope.
+
+Lemma lead_coef_prod (s : seq {poly R}) :
+  \prod_(p <- s) lead_coef p = lead_coef (\prod_(p <- s) p).
+Proof.
+elim: s=> [|a l IHl]; first by rewrite !big_nil lead_coef1.
+by rewrite !big_cons lead_coefM -IHl.
+Qed.
+
+Import GRing.Theory.
+
+Lemma monic_leadVMp (p : {poly R}) : (lead_coef p) \is a GRing.unit ->
+  ((lead_coef p)^-1 *: p) \is monic.
+Proof. by move=> *; apply/monicP; rewrite lead_coefZ mulVr. Qed.
+
+End BigOp.
+
+Section Matrix.
+Import GRing.Theory.
+Local Open Scope ring_scope.
+
+
+Section matrix_Type.
+
+Variable T : Type.
+(**** This lemma is useful to rewrite in a big expression, and it is unsightly
+to do a "have" in a proof for proving that. *********)
+Lemma matrix_comp k l m n (E : 'I_k -> 'I_l -> T) (F : 'I_n -> 'I_k) G :
+  \matrix_(i < n, j < m) ((\matrix_(i0 < k, j0 < l) E i0 j0) (F i) (G j)) =
+  \matrix_(i, j) (E (F i) (G j)).
+Proof. by apply/matrixP=> i j; rewrite !mxE. Qed.
+
+End matrix_Type.
+
+Section matrix_fieldType.
+
+Variable F : fieldType.
+
+(* mx_poly *)
+Lemma horner_mx_dvdp n (p q : {poly F}) (A : 'M_n.+1) :
+  (dvdp p q) -> horner_mx A p = 0 -> horner_mx A q = 0.
+Proof. by case/dvdpP=> r ->; rewrite rmorphM=> /= ->; rewrite mulr0. Qed.
+
+Lemma mxminpolyP n (A : 'M[F]_n.+1) (p : {poly F}) :
+  p \is monic -> horner_mx A p = 0 ->
+  (forall q, horner_mx A q = 0 -> (dvdp p q)) ->
+  p = mxminpoly A.
+Proof.
+move=> pmon eqpA0 pdvq.
+apply/eqP; rewrite -eqp_monic //; last exact: mxminpoly_monic.
+apply/andP; split.
+  by apply/pdvq/mx_root_minpoly.
+exact: mxminpoly_min.
+Qed.
+
+End matrix_fieldType.
+
+Section matrix_ringType.
+Variable R : ringType.
+
+
+Lemma char_block_mx m n (A : 'M[R]_m) (D : 'M[R]_n) B C :
+  char_poly_mx (block_mx A B C D) =
+ block_mx (char_poly_mx A) (map_mx polyC (-B)) 
+          (map_mx polyC (-C)) (char_poly_mx D).
+Proof.
+apply/matrixP=> i j; rewrite !mxE.
+case: splitP=> k eqik; rewrite !mxE; case: splitP=> l eqjmpl; rewrite !mxE;
+rewrite -!(inj_eq (@ord_inj _)) eqik eqjmpl ?eqn_add2l // rmorphN.
+  by rewrite ltn_eqF ?ltn_addr // sub0r.
+by rewrite gtn_eqF ?ltn_addr // sub0r.
+Qed.
+
+Lemma char_dblock_mx m n (A : 'M[R]_m) (B : 'M[R]_n) :
+  char_poly_mx (block_mx A 0 0 B) =
+  block_mx (char_poly_mx A) 0 0 (char_poly_mx B).
+Proof. by rewrite char_block_mx !oppr0 !map_mx0. Qed.
+
+End matrix_ringType.
+
+End Matrix.
+
+Section poly_idomainType.
+
+Variable R : idomainType.
+Import GRing.Theory.
+Local Open Scope ring_scope.
+
+Lemma coprimep_irreducible (p q : {poly R}) : ~~(p %= q) ->
+  irreducible_poly p -> irreducible_poly q -> coprimep p q.
+Proof.
+move=> neqdpq [szpgt1 Heqdp] [szqgt1 Heqdq].
+have gcdvp:= (dvdp_gcdl p q).
+have gcdvq:= (dvdp_gcdr p q).
+case: (altP (size (gcdp p q) =P 1%N))=> [/eqP //|neqsz1].
+have:= (Heqdp _ neqsz1 gcdvp); rewrite eqp_sym /eqp dvdp_gcd.
+case/andP=> [/andP [ _ pdvq]] _.
+have:= (Heqdq _ neqsz1 gcdvq); rewrite eqp_sym /eqp dvdp_gcd.
+case/andP=> [/andP [qdvp _]] _.
+by rewrite /eqp pdvq qdvp in neqdpq.
+Qed.
+
+Lemma irreducible_dvdp_seq (p r : {poly R}) s :
+    irreducible_poly p -> p \is monic -> (dvdp p r) ->
+    (forall q, q \in s -> irreducible_poly q) ->
+    (forall q, q \in s -> q \is monic) ->
+    r = \prod_(t <- s) t ->
+    p \in s.
+Proof.
+move=> pIrr pm.
+elim: s r => [r pdvr _ _|a l IHl r pdvr Irr mon].
+  rewrite big_nil=> eqr1; move: pdvr pIrr.
+  rewrite eqr1 dvdp1 /irreducible_poly=> /eqP ->.
+  by rewrite ltnn; case.
+rewrite big_cons=> eqrM; move: pdvr; rewrite eqrM=> pdvM.
+case: (altP (@idP (eqp p a)))=>[|neqdpa].
+  have am: a \is monic by apply: mon; rewrite mem_head.
+  by rewrite eqp_monic // => /eqP ->; rewrite mem_head.
+have Hia: irreducible_poly a by apply: Irr; rewrite mem_head.
+have cppa := coprimep_irreducible neqdpa pIrr Hia.
+rewrite (Gauss_dvdpr _ cppa) in pdvM.
+apply/mem_behead/(IHl _ pdvM)=> // q qinl.
+  by apply: Irr; rewrite mem_behead.
+by rewrite mon // mem_behead.
+Qed.
+
+Lemma unicity_decomposition (s1 s2 : seq {poly R}) : forall (p : {poly R}),
+  (forall r, r \in s1 -> irreducible_poly r) ->
+  (forall r, r \in s2 -> irreducible_poly r) ->
+  (forall r, r \in s1 -> r \is monic) ->
+  (forall r, r \in s2 -> r \is monic) ->
+  p = \prod_(r <- s1) r ->  p = \prod_(r <- s2) r ->
+  perm_eq s1 s2.
+Proof.
+elim: s1 s2=> [|a1 l1 IHl s2 p Irr1 Irr2 mon1 mon2].
+  case=> // a l p _ Irr2 _ mon2->.
+  rewrite big_nil big_cons=> eq1M.
+  have: irreducible_poly a by apply: Irr2; rewrite mem_head.
+  rewrite /irreducible_poly; case.
+  by rewrite ltnNge leq_eqVlt -dvdp1 eq1M dvdp_mulr.
+rewrite big_cons=> eqpM eqpbig /=.
+have a1ins2: a1 \in s2.
+  apply: (irreducible_dvdp_seq _ _ _ Irr2 mon2 eqpbig).
+  +by apply: Irr1; rewrite mem_head.
+  -by rewrite mon1 // mem_head.
+  by rewrite eqpM dvdp_mulr.
+rewrite perm_sym (perm_trans (perm_to_rem a1ins2)) //.
+rewrite perm_cons perm_sym.
+have nza1: a1 != 0.
+  by apply: irredp_neq0; apply: Irr1; rewrite mem_head.
+rewrite (perm_big _ (perm_to_rem a1ins2)) /= big_cons eqpM in eqpbig.
+have/(mulfI nza1) eqbig := eqpbig.
+set q:= \prod_(j <- l1) j.
+apply: (IHl _ q)=> // r Hr.
+  +by apply: Irr1; rewrite mem_behead.
+  -by apply: Irr2; rewrite (mem_rem Hr).
+  +by rewrite mon1 // mem_behead.
+  -by rewrite mon2 // (mem_rem Hr).
+Qed.
+
+End poly_idomainType.
